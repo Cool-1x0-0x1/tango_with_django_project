ec221cd3e247d5e98a6de8cacb425c4bd08a60b7
Lingjie Jia <441334472@qq.com>
2022-02-05 20:25:03 +0000

    commit 6.0

--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,11 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
